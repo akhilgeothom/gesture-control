13e08c995a8582b9528abc2f240e057b97a8e3b1
akhilgeothom <akhilgeothom@gmail.com>
2019-04-23 11:49:26 +0530

    added .gitignore

--- detect.py
+++ detect.py
@@ -6,27 +6,27 @@ import pyautogui
 
 palm_cascade = cv2.CascadeClassifier('xml/palm.xml')
 
-cap = cv2.VideoCapture(3)
+cap = cv2.VideoCapture(0)
 posx = 0
 posy = 0
 while True:
     ret,img = cap.read()
     grey = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    palms = palm_cascade.detectMultiScale(grey,1.25,6)
-    maxw = maxh = 120
+    palms = palm_cascade.detectMultiScale(grey,1.25,5)
+    maxw = maxh = 90
     curx = cury = 0
     for (x,y,w,h) in palms:
-        cv2.rectangle(img, (x,y), (x+w, y+h), (255, 0, 0), 2)
+        cv2.rectangle(grey, (x,y), (x+w, y+h), (255, 0, 0), 2)
         # print (x,y,w,h)
         if(maxh + maxw < h + w):
             maxh = h
             maxw = w
             curx = x
             cury = y
-    print(curx)
-    if (maxh!=120):
-        cv2.rectangle(img, (curx,cury), (curx+maxw, cury+maxh), (0, 255, 0), 2)
-    cv2.imshow('img',img)
+    # print(curx)
+    if (maxh!=90):
+        cv2.rectangle(grey, (curx,cury), (curx+maxw, cury+maxh), (0, 255, 0), 2)
+    cv2.imshow('img',grey)
 
     if(posx-10 > curx):
         pyautogui.press('left')
